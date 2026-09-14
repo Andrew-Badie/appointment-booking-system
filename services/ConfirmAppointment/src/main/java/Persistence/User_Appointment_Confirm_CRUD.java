1d31407772585e6a0a00d03f84ac6121e59e07ac
Andrew Badie <andrewbadie22@gmail.com>
2026-09-14 13:12:18 -0400

    Remove unused internal JDK import blocking confirmation compilation

--- services/ConfirmAppointment/src/main/java/Persistence/User_Appointment_Confirm_CRUD.java
+++ services/ConfirmAppointment/src/main/java/Persistence/User_Appointment_Confirm_CRUD.java
@@ -1,6 +1,5 @@
 package Persistence;
 
-import static com.sun.org.apache.xalan.internal.lib.ExsltDatetime.date;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
